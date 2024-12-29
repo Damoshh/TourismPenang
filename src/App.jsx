a0abe34c43344229a938e8dcbfd1171dbc3cf738
nishi <nishhhh20@gmail.com>
2024-12-29 14:12:15 +0800

    change formatting in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
@@ -8,30 +8,28 @@ import About from "./pages/About/About";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 
-
 function App() {
+  const current_theme = localStorage.getItem("current_theme");
+  const [theme, setTheme] = useState(current_theme ? current_theme : "light");
 
-    const current_theme = localStorage.getItem('current_theme');
-    const [theme, setTheme] = useState (current_theme ? current_theme : 'light');
+  useEffect(() => {
+    localStorage.setItem("current_theme", theme);
+  }, [theme]);
 
-    useEffect(()=>{
-        localStorage.setItem('current_theme', theme);
-        }, [theme])
-
-    return(
-        <div  className={`App ${theme}`}>
-        <Router>
-            <Navbar theme={theme} setTheme={setTheme} />
-            <Routes>
-                <Route path="/" element = {<Home/>} />
-                <Route path="/event" element = {<Event/>} />
-                <Route path="/place" element = {<Place/>} />
-                <Route path="/about" element = {<About/>} />
-            </Routes>
-            <Footer/>
-        </Router>
-        </div>
-    );
+  return (
+    <div className={`App ${theme}`}>
+      <Router>
+        <Navbar theme={theme} setTheme={setTheme} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/place" element={<Place />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
