ffeeb7a3747940f64e65ba19204aed57117d07e6
Damoshh <adamrujhan@gmail.com>
2025-01-03 22:49:41 +0800

    add entertainment place

--- src/pages/Place/ListPlaces/entertainmentPlaces.js
+++ src/pages/Place/ListPlaces/entertainmentPlaces.js
@@ -1,5 +1,17 @@
-const entertainmentPlaces = [
+import imgEtm1 from '../../../assets/imgEtm1.webp';
+import imgEtm2 from '../../../assets/imgEtm2.jpg';
 
-];
+const entertainmentPlaces = [
+    {
+      id: 31,
+      title: "Escape Theme Park",
+      category: "entertainment",
+      images: [imgEtm1, imgEtm2],
+      location: "Teluk Bahang, Penang",
+      locationLink: "https://maps.app.goo.gl/SE3guCeFxcGVt2dT9",
+      rating: 3
+    }
+
+  ];
 
 export default entertainmentPlaces;
