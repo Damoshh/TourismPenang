940b6b202ddf1589c05bd62d65f76434af46487a
nishi <nishhhh20@gmail.com>
2025-01-09 02:06:05 +0800

    update list food

--- src/pages/Place/ListPlaces/foodPlaces.js
+++ src/pages/Place/ListPlaces/foodPlaces.js
@@ -6,6 +6,24 @@ import imgRst7 from '../../../assets/imgRst7.jpg';
 import imgRst8 from '../../../assets/imgRst8.jpg';
 import imgRst9 from '../../../assets/imgRst9.jpg';
 import imgRst10 from '../../../assets/imgRst10.jpg';
+import imgRst11 from '../../../assets/imgRst11.jpg';
+import imgRst12 from '../../../assets/imgRst12.jpg';
+import imgRst13 from '../../../assets/imgRst13.jpg';
+import imgRst14 from '../../../assets/imgRst14.jpg';
+import imgRst15 from '../../../assets/imgRst15.jpg';
+import imgRst16 from '../../../assets/imgRst16.jpg';
+import imgRst17 from '../../../assets/imgRst17.jpg';
+import imgRst18 from '../../../assets/imgRst18.jpg';
+import imgRst19 from '../../../assets/imgRst19.jpg';
+import imgRst20 from '../../../assets/imgRst20.jpg';
+import imgRst21 from '../../../assets/imgRst21.jpg';
+import imgRst22 from '../../../assets/imgRst22.jpg';
+import imgRst23 from '../../../assets/imgRst23.jpg';
+import imgRst24 from '../../../assets/imgRst24.jpg';
+import imgRst25 from '../../../assets/imgRst25.jpg';
+import imgRst26 from '../../../assets/imgRst26.jpg';
+
+
 
 const foodPlaces = [
     {
@@ -43,8 +61,79 @@ const foodPlaces = [
       location: "Jelutong, Penang",
       locationLink: "https://maps.app.goo.gl/JcPr8vPaJz5JMZm46",
       rating: 3
+    },    
+    {
+      id: 25,
+      title: "Sister Curry Mee",
+      category: "food",
+      images: [imgRst11, imgRst12],
+      location: "Ayer Itam, Penang",
+      locationLink: "https://maps.app.goo.gl/H21LSEDRmGaq6tcw6",
+      rating: 4
+    },    
+    {
+      id: 26,
+      title: "Alamak Cafe",
+      category: "food",
+      images: [imgRst13, imgRst14],
+      location: "George Town, Penang",
+      locationLink: "https://maps.app.goo.gl/SRkt5DYn41yLAc329",
+      rating: 4
+    },    
+    {
+      id: 27,
+      title: "TheFat Burger",
+      category: "food",
+      images: [imgRst15, imgRst16],
+      location: "Bayan Baru, Penang",
+      locationLink: "https://maps.app.goo.gl/VDPLmM8zF1TE6GWR8",
+      rating: 5
+    },    
+    {
+      id: 28,
+      title: "Kota Dine & Coffee",
+      category: "food",
+      images: [imgRst17, imgRst18],
+      location: "George Town, Penang",
+      locationLink: "https://maps.app.goo.gl/AeZmrY6pZf4MYHT8A",
+      rating: 5
+    },    
+    {
+      id: 29,
+      title: "Rumah Kacha Cafe",
+      category: "food",
+      images: [imgRst19, imgRst20],
+      location: "George Town, Penang",
+      locationLink: "https://maps.app.goo.gl/WmCALjtSRWKYWLdz8",
+      rating: 4
+    },    
+    {
+      id: 30,
+      title: "The Warung Cafe",
+      category: "food",
+      images: [imgRst21, imgRst22],
+      location: "George Town, Penang",
+      locationLink: "https://maps.app.goo.gl/SSxKHTtB5GrZax1t5",
+      rating: 4
     },
-
+    {
+      id: 31,
+      title: "Nasi Kandar Kampung Melayu",
+      category: "food",
+      images: [imgRst23, imgRst24],
+      location: "Ayer Itam, Penang",
+      locationLink: "https://maps.app.goo.gl/YKXbRH42YKeG7v7JA",
+      rating: 4
+    },
+    {
+      id: 32,
+      title: "Laksa Janggus",
+      category: "food",
+      images: [imgRst25, imgRst26],
+      location: "Balik Pulau, Penang",
+      locationLink: "https://maps.app.goo.gl/uXpysZj1S1QujgkbA",
+      rating: 3
+    },
   ];
   
 export default foodPlaces;
